cdc73d5dc20604f50525a2d430bdf165f0cec991
JelmerBatenburg <jelmer.batenburg@gmail.com>
2019-12-02 14:01:52 +0100

    astar

--- Unity Codes/Assets/AStar/Script/AStar.cs
+++ Unity Codes/Assets/AStar/Script/AStar.cs
@@ -13,8 +13,8 @@ public class AStar : MonoBehaviour
     public float floorCheckHeight;
     public bool test;
     public Transform testStartPos, testEndpos;
-    List<int> pathNodes = new List<int>();
-    List<int> badPathNodes = new List<int>();
+    public float checkValue;
+    public Vector3[] finalPath = new Vector3[0];
 
     public void Start()
     {
@@ -26,7 +26,7 @@ public class AStar : MonoBehaviour
     {
         if (test)
         {
-            StartCoroutine(GetPath(testStartPos.position, testEndpos.position));
+            finalPath = GetPath(testStartPos.position, testEndpos.position);
             test = false;
         }
     }
@@ -57,27 +57,100 @@ public class AStar : MonoBehaviour
 
                 for (int heightOffset = 1; heightOffset < heightAmount; heightOffset++)
                     if (i + (heightOffset * yOffset) < nodes.Count)
-                        if (Physics.CheckBox(nodes[i + (heightOffset * yOffset)].position, Vector3.one * tileSize / 2, Quaternion.identity, checkMask))
+                        if (Physics.CheckBox(nodes[i + (heightOffset * yOffset)].position, Vector3.one * tileSize / 2f, Quaternion.identity, checkMask))
                             isFloor = false;
                 nodes[i].walkable = isFloor;
             }
     }
 
-    public IEnumerator GetPath(Vector3 startPos, Vector3 endPos)
+    public Vector3[] GetPath(Vector3 startPos, Vector3 endPos)
+    {
+        List<int> pathNodes = new List<int>();
+        List<int> closedList = new List<int>();
+        int currentNode = GetClosestTileIndex(startPos);
+        int endIndex = GetClosestTileIndex(endPos);
+        pathNodes.Add(currentNode);
+        closedList.Add(currentNode);
+
+        while (currentNode != endIndex)
+        {
+            closedList.Add(currentNode);
+            int[] neighboringNodes = GetSurroundingNodes(currentNode);
+            foreach(int node in neighboringNodes)
+            {
+                if (closedList.Contains(node))
+                    continue;
+
+                if (pathNodes.Contains(node) && nodes[node].arrivalCost <= nodes[currentNode].arrivalCost + 1)
+                {
+                    nodes[node].arrivalCost = nodes[currentNode].arrivalCost + 1;
+                    continue;
+                }
+
+                nodes[node].arrivalCost = nodes[currentNode].arrivalCost + 1;
+                nodes[node].distanceCost = Vector3.Distance(nodes[endIndex].position, nodes[node].position);
+                pathNodes.Add(node);
+            }
+            if (pathNodes.Count == closedList.Count)
+                break;
+
+            currentNode = GetLowestCost(pathNodes.ToArray(), closedList);
+        }
+        closedList.Add(currentNode);
+
+        return GetFinalPath(closedList);
+    }
+
+    public Vector3[] GetFinalPath(List<int> usedTiles)
+    {
+        List<Vector3> pathNodes = new List<Vector3>();
+        int currentNode = usedTiles[usedTiles.Count - 1];
+        while(currentNode != usedTiles[0])
+        {
+            pathNodes.Add(nodes[currentNode].position);
+
+            int[] neighbouringNodes = GetSurroundingNodes(currentNode);
+            int lowest = 0;
+            foreach (int node in neighbouringNodes)
+                if (usedTiles.Contains(node))
+                    if (lowest == 0 || nodes[node].arrivalCost <= nodes[lowest].arrivalCost)
+                        lowest = node;
+
+            currentNode = lowest;
+        }
+        pathNodes.Reverse();
+        return pathNodes.ToArray();
+    }
+
+    public int GetLowestCost(int[] checkNodes, List<int> closedNodes)
+    {
+        int lowest = 0;
+
+        foreach (int node in checkNodes)
+            if (nodes[lowest].totalCost == 0 || nodes[node].totalCost <= nodes[lowest].totalCost && !closedNodes.Contains(node))
+                lowest = node;
+
+        return lowest;
+    }
+
+    public int[] GetSurroundingNodes(int node)
     {
         int yOffset = Mathf.RoundToInt(nodeAmount.x);
         int zOffset = Mathf.RoundToInt(nodeAmount.x * nodeAmount.y);
-        pathNodes = new List<int>();
-        badPathNodes = new List<int>();
-        int currentNode = GetClosestTileIndex(startPos);
-        pathNodes.Add(currentNode);
+        List<int> checkNodes = new List<int>();
 
-        while(currentNode != GetClosestTileIndex(endPos))
-        {
-            
-        }
+        for (int x = -1; x <= 1; x++)
+            for (int y = -1; y <= 1; y++)
+                for (int z = -1; z <= 1; z++)
+                {
+                    if ((Mathf.Abs(x) + Mathf.Abs(y) + Mathf.Abs(z)) == 3)
+                        continue;
 
-        yield return null;
+                    int checkIndex = node + x + (y * yOffset) + (z * zOffset);
+                    if (checkIndex < nodes.Count && checkIndex >= 0 && nodes[checkIndex].walkable && Vector3.Distance(nodes[node].position, nodes[checkIndex].position) <= (tileSize * 3))
+                        checkNodes.Add(checkIndex);
+                }
+        return checkNodes.ToArray();
     }
 
     public int GetClosestTileIndex(Vector3 pos)
@@ -97,13 +170,14 @@ public class AStar : MonoBehaviour
         Gizmos.DrawWireCube(transform.position, area);
         foreach(Node node in nodes)
         {
-            Gizmos.color = node.obstructed ? Color.black : Color.white;
+            Gizmos.color = node.obstructed ? Color.black : new Color(1, 1, 1, 0.1f);
             if (node.walkable)
                 Gizmos.DrawSphere(node.position, tileSize / 2);
             Gizmos.DrawWireCube(node.position, Vector3.one * tileSize);
         }
-        foreach (int index in pathNodes)
-            Gizmos.DrawSphere(nodes[index].position, tileSize / 2f);
+        Gizmos.color = Color.red;
+        foreach (Vector3 pos in finalPath)
+            Gizmos.DrawSphere(pos, tileSize / 2f);
     }
 
     public class Node
@@ -114,7 +188,7 @@ public class AStar : MonoBehaviour
         public float distanceCost;
         public float totalCost
         {
-            get { return arrivalCost - distanceCost;}
+            get { return arrivalCost + distanceCost;}
         }
 
         public Node(Vector3 _position, bool _obstructed)
